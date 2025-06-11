e7f74b7b0700fc38b835e521da8f031a49a326dc
hzeyuan <599012428@qq.com>
2025-06-12 01:41:45 +0800

    fix

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -10,7 +10,15 @@ git checkout ${COMFYUI_VERSION}
 python3 -m venv --system-site-packages venv
 source venv/bin/activate
 
+# Install torch, xformers and sageattention
+pip3 install --no-cache-dir torch=="${TORCH_VERSION}" torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip3 install --no-cache-dir xformers=="${XFORMERS_VERSION}" --index-url https://download.pytorch.org/whl/cu121
 
+# Install requirements
+pip3 install -r requirements.txt
+pip3 install accelerate
+pip3 install sageattention
+pip install setuptools --upgrade
 
 # Install ComfyUI Custom Nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
@@ -19,40 +27,40 @@ pip3 install -r requirements.txt
 
 # ComfyUI-Advanced-ControlNet
 git clone https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git \
-  /workspace/ComfyUI/custom_nodes/ComfyUI-Advanced-ControlNet
+  /ComfyUI/custom_nodes/ComfyUI-Advanced-ControlNet
 
 
 # comfyui_controlnet_aux
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git \
-  /workspace/ComfyUI/custom_nodes/comfyui_controlnet_aux
+  /ComfyUI/custom_nodes/comfyui_controlnet_aux
 
-cd /workspace/ComfyUI/custom_nodes/comfyui_controlnet_aux
+cd /ComfyUI/custom_nodes/comfyui_controlnet_aux
 pip3 install -r requirements.txt
 
  # comfyui-inpaint-nodes
 git clone https://github.com/Acly/comfyui-inpaint-nodes.git \
-  /workspace/ComfyUI/custom_nodes/comfyui-inpaint-nodes
+  /ComfyUI/custom_nodes/comfyui-inpaint-nodes
 
 # masquerade-nodes-comfyui
 git clone https://github.com/BadCafeCode/masquerade-nodes-comfyui.git \
-  /workspace/ComfyUI/custom_nodes/masquerade-nodes-comfyui
+  /ComfyUI/custom_nodes/masquerade-nodes-comfyui
 
 # ComfyUI-Florence2
 git clone https://github.com/kijai/ComfyUI-Florence2.git \
-  /workspace/ComfyUI/custom_nodes/ComfyUI-Florence2
+  /ComfyUI/custom_nodes/ComfyUI-Florence2
 
-cd /workspace/ComfyUI/custom_nodes/ComfyUI-Florence2
+cd /ComfyUI/custom_nodes/ComfyUI-Florence2
 pip3 install -r requirements.txt
 
 # ComfyUI-segment-anything-2
 git clone https://github.com/kijai/ComfyUI-segment-anything-2.git \
-  /workspace/ComfyUI/custom_nodes/ComfyUI-segment-anything-2
+  /ComfyUI/custom_nodes/ComfyUI-segment-anything-2
 
 # ComfyUI_essentials
 git clone https://github.com/cubiq/ComfyUI_essentials.git \
-  /workspace/ComfyUI/custom_nodes/ComfyUI_essentials
+  /ComfyUI/custom_nodes/ComfyUI_essentials
 
-cd /workspace/ComfyUI/custom_nodes/ComfyUI_essentials
+cd /ComfyUI/custom_nodes/ComfyUI_essentials
 pip3 install -r requirements.txt
 
 # Install ComfyUI-FastAPI
@@ -63,19 +71,6 @@ pip3 install -r requirements.txt
 
 pip3 cache purge
 
-# Install torch, xformers and sageattention
-pip3 install --no-cache-dir torch=="${TORCH_VERSION}" torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip3 install --no-cache-dir xformers=="${XFORMERS_VERSION}" --index-url https://download.pytorch.org/whl/cu121
-
-
-pip3 install accelerate
-pip3 install sageattention
-pip3 install numpy==1.26.4
-pip install setuptools --upgrade
-
-# Install requirements
-pip3 install -r requirements.txt
-
 # Fix some incorrect modules
-
+pip3 install numpy==1.26.4
 deactivate
