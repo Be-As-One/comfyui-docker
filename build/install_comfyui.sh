8ed029688153cd3ac498e181f5f97def847e710f
hzeyuan <599012428@qq.com>
2025-06-11 20:37:43 +0800

    fix

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -25,8 +25,49 @@ git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-M
 cd custom_nodes/ComfyUI-Manager
 pip3 install -r requirements.txt
 
-chmod +x ./undress_custom_nodes.sh
-./undress_custom_nodes.sh
+git clone https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git \
+  /workspace/ComfyUI/custom_nodes/ComfyUI-Advanced-ControlNet
+
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-Advanced-ControlNet
+pip3 install -r requirements.txt
+
+
+
+git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git \
+  /workspace/ComfyUI/custom_nodes/comfyui_controlnet_aux
+
+cd /workspace/ComfyUI/custom_nodes/comfyui_controlnet_aux
+pip3 install -r requirements.txt
+
+git clone https://github.com/Acly/comfyui-inpaint-nodes.git \
+  /workspace/ComfyUI/custom_nodes/comfyui-inpaint-nodes
+
+cd /workspace/ComfyUI/custom_nodes/comfyui-inpaint-nodes
+pip3 install -r requirements.txt
+
+git clone https://github.com/BadCafeCode/masquerade-nodes-comfyui.git \
+  /workspace/ComfyUI/custom_nodes/masquerade-nodes-comfyui
+
+cd /workspace/ComfyUI/custom_nodes/masquerade-nodes-comfyui
+pip3 install -r requirements.txt
+
+git clone https://github.com/kijai/ComfyUI-Florence2.git \
+  /workspace/ComfyUI/custom_nodes/ComfyUI-Florence2
+
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-Florence2
+pip3 install -r requirements.txt
+
+git clone https://github.com/kijai/ComfyUI-segment-anything-2.git \
+  /workspace/ComfyUI/custom_nodes/ComfyUI-segment-anything-2
+
+cd /workspace/ComfyUI/custom_nodes/ComfyUI-segment-anything-2   
+pip3 install -r requirements.txt
+
+git clone https://github.com/cubiq/ComfyUI_essentials.git \
+  /workspace/ComfyUI/custom_nodes/ComfyUI_essentials
+
+cd /workspace/ComfyUI/custom_nodes/ComfyUI_essentials
+pip3 install -r requirements.txt
 
 # Install ComfyUI-FastAPI
 cd /ComfyUI
