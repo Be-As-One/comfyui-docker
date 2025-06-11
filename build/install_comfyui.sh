6c859a8f5ef55c94facc3c0b090eac984ee19786
hzeyuan <599012428@qq.com>
2025-06-12 01:32:12 +0800

    fix

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -10,15 +10,7 @@ git checkout ${COMFYUI_VERSION}
 python3 -m venv --system-site-packages venv
 source venv/bin/activate
 
-# Install torch, xformers and sageattention
-pip3 install --no-cache-dir torch=="${TORCH_VERSION}" torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip3 install --no-cache-dir xformers=="${XFORMERS_VERSION}" --index-url https://download.pytorch.org/whl/cu121
 
-# Install requirements
-pip3 install -r requirements.txt
-pip3 install accelerate
-pip3 install sageattention
-pip install setuptools --upgrade
 
 # Install ComfyUI Custom Nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
@@ -71,6 +63,19 @@ pip3 install -r requirements.txt
 
 pip3 cache purge
 
-# Fix some incorrect modules
+# Install torch, xformers and sageattention
+pip3 install --no-cache-dir torch=="${TORCH_VERSION}" torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip3 install --no-cache-dir xformers=="${XFORMERS_VERSION}" --index-url https://download.pytorch.org/whl/cu121
+
+
+pip3 install accelerate
+pip3 install sageattention
 pip3 install numpy==1.26.4
+pip install setuptools --upgrade
+
+# Install requirements
+pip3 install -r requirements.txt
+
+# Fix some incorrect modules
+
 deactivate
